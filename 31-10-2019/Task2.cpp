e427301792a6c78e1c383f2eeb112a51c46cfb36
Momchil Todorov <mom.todorov.tues19@gmail.com>
2019-10-31 11:50:04 +0200

    Update Task2.cpp

--- 31-10-2019/Task2.cpp
+++ 31-10-2019/Task2.cpp
@@ -4,6 +4,25 @@ using namespace std;
 
 int main()
 {
+    int num, result = 1;
 
+    do
+    {
+        cin >> num;
+    }while(num<0);
+
+    if(num==0||num==1)result = 1;
+    else
+    {
+        for(int i = 1, fib0 = 1, fib1 = 1; i < num; i++)
+        {
+            result = fib0 + fib1;
+            fib0 = fib1;
+            fib1 = result;
+        }
+    }
+
+    cout << result << endl;
+    
     return 0;
 }
