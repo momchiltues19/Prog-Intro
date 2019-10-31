b519fc9328abd281fba7e81238937953db324069
Momchil Todorov <mom.todorov.tues19@gmail.com>
2019-10-31 11:49:31 +0200

    Update Task3.cpp

--- 31-10-2019/Task3.cpp
+++ 31-10-2019/Task3.cpp
@@ -4,6 +4,34 @@ using namespace std;
 
 int main()
 {
+    int num, row;
 
+    do
+    {
+        cin >> num;
+    } while(num<=0);
+
+    row = 2*num-1;
+
+    for(int i = 0; i < num; i++)
+    {
+        for(int j = 0; j < row; j++)
+        {
+            if((row-j >= num-i) && (row-j <= num+i))
+                cout << '*';
+            else
+                cout << ' ';
+        }
+        cout << endl;
+    }
+
+    for(int j = 0; j < row; j++)
+    {
+        if((row-j >= num) && (row-j <= num))
+            cout << '*';
+        else
+            cout << ' ';
+    }
+    
     return 0;
 }
