ed288cbb11490581a149aa24fea21a7c0cec23f3
Momchil Todorov <mom.todorov.tues19@gmail.com>
2019-10-31 11:48:05 +0200

    Update Task6.cpp

--- 31-10-2019/Task6.cpp
+++ 31-10-2019/Task6.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
+    int num, binary[100], b_size = 0;
 
+    cin >> num;
+
+    for(; num>0; b_size++)
+    {
+        binary[b_size] = num%2;
+        num /= 2;
+    }
+
+    for(int i=b_size; i>0; i--)
+    {
+        cout << binary[i-1];
+    }
+    
     return 0;
 }
