a271c0ab358762a0c3c29c8a90f44d517a425dda
Momchil Todorov <mom.todorov.tues19@gmail.com>
2019-10-31 11:48:33 +0200

    Update Task5.cpp

--- 31-10-2019/Task5.cpp
+++ 31-10-2019/Task5.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
+    int num, a[100];
+    bool flag = 1;
 
+    cin >> num;
+
+    for(int i=0; i<num; i++)
+    {
+        cin >> a[i];
+    }
+
+    for(int i=1; i<num-1; i++)
+    {
+        if(!((a[i]>a[i-1])&&(a[i+1]<a[i]))&&!((a[i]<a[i-1])&&(a[i+1]>a[i])))
+        {
+            flag = 0;
+            break;
+        }
+    }
+    cout << boolalpha;
+    cout << flag;
+    
     return 0;
 }
