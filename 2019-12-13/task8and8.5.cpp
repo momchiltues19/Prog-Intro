d39f94f355e4788c304f2a154aaf2e45a87446ea
Momchil Todorov <mom.todorov.tues19@gmail.com>
2019-12-13 16:01:14 +0200

    Create task8and8.5.cpp

---
+++ 2019-12-13/task8and8.5.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int count_a_words(string);
+
+int main()
+{
+    string input, check;
+
+    getline(cin, input);
+
+    //cout << boolalpha;
+    cout << count_a_words(input);
+
+    return 0;
+}
+
+int count_a_words(string s)
+{
+    int result = 0, n=s.length();
+
+    for(int i=0; i<n; i++)
+    {
+        if(isalnum(s[i])&&!isalnum(s[i-1]))
+        {
+            while(isalnum(s[i]))
+            {
+                if(s[i]=='a'||s[i]=='A')result++;
+                i++;
+            }
+        }
+    }
+
+    return result;
+}
