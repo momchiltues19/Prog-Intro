05fb526f832e0b540311c7ac2d79827b2fdbf529
Momchil Todorov <mom.todorov.tues19@gmail.com>
2019-12-20 15:53:41 +0200

    Create task3,75-unfinished.cpp

---
+++ 2019-12-20 - Recursion/task3,75-unfinished.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int m_power(int, int, int);
+
+int main()
+{
+    int num, pow, mod;
+
+    cin >> num;
+    cin >> pow;
+    cin >> mod;
+
+    cout << m_power(num, pow, mod) << endl;
+
+    return 0;
+}
+
+int m_power(int n, int a, int m)
+{
+    int temp;
+    if(a==0) return 1;
+    if(a%2==0){temp = m_power(n, a/2, m); return (temp*temp)%m;}
+    return (n%m)*(m_power(n,a-1,m))%m;
+}
